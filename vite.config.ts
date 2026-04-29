294c9cd121e2c7bbd35ce8afc858dee3bca95adf
Francisco Rodrigues <francisco.rodrigues@cern.ch>
2026-04-29 12:09:22 +0200

    Build both index.html and background.html

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,14 @@ export default defineConfig({
     server: {
         cors: {
             origin: "https://www.owlbear.rodeo",
+        },
+    },
+    build: {
+        rolldownOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                background: resolve(__dirname, "background.html"),
+            },
         },
     },
 });
